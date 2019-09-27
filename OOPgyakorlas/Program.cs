dae70673e42061af8756b09bff1bd731b541676b
Leora <niki.torok@gmail.com>
2019-09-27 12:44:38 +0200

    2. feladat

--- OOPgyakorlas/Program.cs
+++ OOPgyakorlas/Program.cs
@@ -16,6 +16,12 @@ namespace AukcioProjekt
             festmenyLista.Add(new Festmeny("Kacsák", "Papp László", "Futurizmus"));
             ketob();
             beolvas("festmenyek.csv");
+            randomLicit();
+            felhasznaloLicit();
+            for (int i = 0; i < festmenyLista.Count; i++)
+            {
+                Console.WriteLine(festmenyLista[i]);
+            }
         }
         public static void ketob()
         {
@@ -49,6 +55,66 @@ namespace AukcioProjekt
             catch (Exception)
             {
 
+                throw;
+            }
+
+        }
+        public static void randomLicit()
+        {
+            
+            for (int i = 0; i < 20; i++)
+            {
+                Random rnd= new Random();
+                int random = rnd.Next(0, festmenyLista.Count);
+                festmenyLista[random].Licit();
+            }
+        }
+        public static void felhasznaloLicit()
+        {
+            int mertek;
+            int sorszam=1;
+            while (sorszam!=0)
+            {
+                do
+                {
+                    try
+                    {
+                        Console.Write("Kérem adja meg a festmény sorszámát: ");
+                        sorszam = int.Parse(Console.ReadLine());
+                        if (sorszam - 1 > festmenyLista.Count)
+                        {
+                            Console.WriteLine("Hiba! nincs ilyen sorszam!!\nPróbálja újra!");
+                        }
+                        if (festmenyLista[sorszam - 1].getElkelt())
+                        {
+                            Console.WriteLine("Hiba a festmény már elkelt!\nKérem adjon meg új sorszámot.");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Hiba!! Nem számot adott meg!!");
+                        Console.ReadKey();
+                        throw;
+                    }
+                } while (sorszam - 1 > festmenyLista.Count&&festmenyLista[sorszam-1].getElkelt());
+            }
+            try
+            {
+                Console.Write("Milyen mértékkel szeretne licitálni?: ");
+                string adat = Console.ReadLine();
+                if (adat.Equals(""))
+                {
+                    mertek = 10;
+                }
+                else
+                {
+                    mertek = int.Parse(adat);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hiba!! Nem számot adott meg!!");
+                Console.ReadKey();
                 throw;
             }
         }
